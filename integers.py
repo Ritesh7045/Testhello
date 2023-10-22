4f9801dd594cca75195ae597ddd4711a59a4676b
Ritesh <gaikwadritesh2804@gmail.com>
2023-10-22 06:26:40 +0000

    Some chnages has been done

--- integers.py
+++ integers.py
@@ -1,8 +1,8 @@
 x = 1
 y = 35656222554887711
 z = -3255522
-
+a = 4
 print(type(x))
 print(type(y))
 print(type(z))
-
+print(type(a))
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 x = 5
 y = "John"
+z = 89
 print(x)
 print(y)
-
+print(z)
